f3ed2419b7d85c73f90dfe1cace80b4716e08af6
Ankur Chavan <ankurchavan1@gmail.com>
2023-03-12 04:34:16 -0400

    Update dijkstra_Ankur_Chavan.py
    Defined Obstacle Space with in built clearance

--- dijkstra_Ankur_Chavan.py
+++ dijkstra_Ankur_Chavan.py
@@ -6,7 +6,20 @@ import math
 import sys
 import numpy as np
 
-
+# defining the obstacle space using half equations
+def obstacle_space(x,y):
+    obs_present = 0
+    if (x <= 5) and (x >= 595) and (y <=5) and (y >= 245):
+        obs_present=1
+    if x >= 95 and x <= 155 and y>= 145 and y <= 255:
+        obs_present=1
+    if (x >= 95) and (x <= 155) and (y>=0) and (y <= 105):
+        obs_present=1
+    if x >= 230.05 and x <= 369.95 and (((y - 165.38) - ((165.38 - 205.77)/ (230.05 - 300))* (x - 230.05)) <= 0) and (((y - 205.77) - ((205.77 - 165.38)/(300 - 369.95))* (x - 300)) <= 0) and (((y - 84.61) - ((84.61 - 44.22)/ (369.95 - 300))* (x - 369.95)) >= 0) and (((y - 44.22) - ((44.22 - 84.61)/ (300 - 230.04))* (x - 300)) >= 0):
+        obs_present=1
+    if x>= 455 and (((y - 246.18) - ((246.18 - 125)/ (455 - 515.59))*(x - 455)) <= 0) and (((y - 125) - ((125 - 3.81)/ (515.19 - 455))*(x - 515.59)) >= 0):
+        obs_present=1
+    return obs_present
 
 
 print("Enter robot parameters")
